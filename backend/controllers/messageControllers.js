7ef49b36e0a0346b0345a13a987c4db01bb0acdc
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-03-06 23:36:36 -0500

    created message route & controller files and created sendMesssage function

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,1 +1,41 @@
-const express = require;
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+
+  let newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    let message = await Message.create(newMessage);
+
+    message = await message.populate("sender", "name, avatar");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name avatar email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    });
+
+    res.json(message);
+  } catch (err) {
+    res.status(400);
+    throw new Error(err.message);
+  }
+});
+
+module.exports = { sendMessage };
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { sendMessage } = require("../controllers/messageControllers");
 const requireAuth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// router.post("/", requireAuth, sendMessage);
+router.post("/", requireAuth, sendMessage);
 // router.get("/:chatId", requireAuth, allMessages);
 
 module.exports = router;
