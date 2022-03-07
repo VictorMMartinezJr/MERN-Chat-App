efa816343618094d0405a092d1d92b9170a93c10
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-03-06 23:42:25 -0500

    created allMessages function in messagesController to fetch all messages of a chat

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { sendMessage } = require("../controllers/messageControllers");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers");
 const requireAuth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", requireAuth, sendMessage);
-// router.get("/:chatId", requireAuth, allMessages);
+router.get("/:chatId", requireAuth, allMessages);
 
 module.exports = router;
