e7b0b344f8d73cff65f4bf1546f414f7980eb416
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-03-15 17:33:44 -0400

    make it better

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 
 dotenv.config();
 connectToDB();
@@ -13,31 +14,31 @@ const app = express();
 // Accept json data
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // ------------- Deployment ------------- //
 
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile();
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running!");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running!!");
+  });
+}
 
 // ------------- Deployment ------------- //
 
+app.get("/", (req, res) => {
+  res.send("API is running!!!");
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
